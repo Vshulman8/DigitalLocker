6f2f01e1e0b6457b271fa706e0e370700e139ca4
Vshulman8 <victoriashulman027@gmail.com>
2025-05-30 15:45:07 +0000

    commit

--- Entry.java
+++ Entry.java
@@ -1,1 +1,23 @@
+public class Entry{
+    private String service;
+    private String username;
+    private String password;
 
+    public Entry(String service, String username, String password){
+      this.service = service;
+      this.username = username;
+      this.password = password;}
+
+    public String getService(){
+        return service;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String toFileString(){
+        return service + "," + username + "," + password;
+    }
+}
